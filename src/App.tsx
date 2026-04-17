99c5d34d504734e654f2a94e6d5aa606ba0a922a
TacoGlz <axeldlg1305@gmail.com>
2026-04-16 21:17:51 -0600

    endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
   const [clientSecret, setClientSecret] = useState<string | undefined>(undefined);
 
 useEffect(() => {
-  fetch("http://localhost:3000/payments/create-payment-intent", {
+  fetch(`${import.meta.env.VITE_API_URL}/payments/create-intent`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ amount: 5000 }), // $50.00 MXN
